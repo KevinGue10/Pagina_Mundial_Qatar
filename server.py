4bb425276a1e5852022162fe66d8e3bf2db93858
gcuriel02 <gcuriel@uninorte.edu.co>
2022-11-25 02:55:35 -0500

    frontend configuracion listo?

--- server.py
+++ server.py
@@ -39,3 +39,28 @@ def equipos():
 @app.route('/jugadores')
 def jugadores():
     return render_template('jugadores.html')
+
+
+@app.route('/editequipos')
+def edit_equipos():
+    return render_template('edit_equipos.html')
+
+@app.route('/editjugadores')
+def edit_jugadores():
+    return render_template('edit_jugadores.html')
+
+@app.route('/arbitros')
+def arbitros():
+    return render_template('arbitros.html')
+
+@app.route('/editarbitros')
+def edit_arbitros():
+    return render_template('edit_arbitros.html')
+
+@app.route('/estadios')
+def estadios():
+    return render_template('estadios.html')
+
+@app.route('/editestadios')
+def edit_estadios():
+    return render_template('edit_estadios.html')
