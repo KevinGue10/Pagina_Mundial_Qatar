ba4089e2cbf5aaea53d3aad8b02ed0c0d536c9fa
sebastiancruz <sebastiancruz@uninorte.edu.co>
2022-11-27 18:32:26 -0500

    final

--- partidos.py
+++ partidos.py
@@ -3,13 +3,13 @@ import datetime
 
 current_time=datetime.datetime.now().replace(microsecond=0)
 
-def get_Fecha(db):    
+def get_Fecha(db):
     fecha=""
     db.execute("SELECT Fecha_Hora FROM Pagina_Mundial.Programacion ORDER BY Fecha_Hora")
     fecha = db.fetchone()
     return fecha
 
-def get_Local(db):    
+def get_Local(db):
     fecha=""
     db.execute("SELECT Fecha_Hora FROM Pagina_Mundial.Programacion ORDER BY Fecha_Hora")
     fecha = db.fetchone()
